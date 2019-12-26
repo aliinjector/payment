40fe835f20d6d1ad7453bd6674b11fa618d2038d
hassan khosrojerdi <hassankhosrojerdi@yahoo.com>
2019-12-26 17:01:20 +0330

    model has been added

---
+++ app/Compare.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
+class Compare extends Model {
+    use SoftDeletes;
+
+    protected $guarded = ['id'];
+    protected $dates = ['deleted_at'];
+
+    public function products() {
+        return $this->belongsToMany('App\Product');
+    }
+
+    public function shop() {
+        return $this->belongsTo('App\Shop');
+    }
+    
+    public function user() {
+        return $this->belongsTo('App\User');
+    }
+}
