5513d4ab6c09b1a943169fc1d5d33ba8854cb588
hassan khosrojerdi <hassankhosrojerdi@yahoo.com>
2019-12-26 16:55:48 +0330

    model has been changed

--- app/wishlist.php
+++ app/wishlist.php
@@ -3,8 +3,25 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Wishlist extends Model
 {
-    //
+  use SoftDeletes;
+  protected $guarded = ['id'];
+  protected $dates = ['deleted_at'];
+
+  public function products()
+  {
+      return $this->belongsToMany('App\Product');
+  }
+  public function shop()
+  {
+      return $this->belongsTo('App\Shop');
+  }
+  public function user()
+  {
+      return $this->belongsTo('App\User');
+  }
 }
