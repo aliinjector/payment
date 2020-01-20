6673bdf673dac42d78d9df6597f59c61b54b22de
hassan khosrojerdi <hassankhosrojerdi@yahoo.com>
2020-01-20 18:10:35 +0330

    sats js has been refactored

--- resources/views/dashboard/shop/stats.blade.php
+++ resources/views/dashboard/shop/stats.blade.php
@@ -1,9 +1,5 @@
 @extends('dashboard.layouts.master')
 @section('content')
-<link href="/dashboard/assets/plugins/datatables/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css">
-<link href="/dashboard/assets/plugins/datatables/buttons.bootstrap4.min.css" rel="stylesheet" type="text/css">
-<link href="/dashboard/assets/plugins/datatables/responsive.bootstrap4.min.css" rel="stylesheet" type="text/css">
-<link href="/dashboard/assets/css/dropify.min.css" rel="stylesheet" type="text/css">
 <div class="page-content">
     <div class="container-fluid">
         <!-- Page-Title -->
@@ -189,52 +185,8 @@
     <!-- Attachment Modal -->
     @endsection
     @section('pageScripts')
-    <script src="/dashboard/assets/plugins/datatables/jquery.dataTables.min.js"></script>
-    <script src="/dashboard/assets/plugins/datatables/dataTables.bootstrap4.min.js"></script>
-    <script src="/dashboard/assets/plugins/datatables/dataTables.buttons.min.js"></script>
-    <script src="/dashboard/assets/plugins/datatables/dataTables.responsive.min.js"></script>
-    <script src="/dashboard/assets/plugins/datatables/responsive.bootstrap4.min.js"></script>
-    <script src="/dashboard/assets/plugins/datatables/jquery.datatable.init.js"></script>
-    <script src="/dashboard/assets/plugins/dropify/js/dropify.min.js"></script>
-    <script src="/dashboard/assets/pages/jquery.form-upload.init.js"></script>
-    <script type="text/javascript">
-        $(window).resize(function() {
-            if ($(window).width() < 1300) {
-                $("body").addClass('enlarge-menu');
+      <script src="{{ asset('/dashboard/assets/js/admin-sats.js') }}"></script>
 
-            } else {
-                $("body").removeClass('enlarge-menu');
-
-            }
-        }).resize();
-    </script>
-    <script type="text/javascript">
-    $(window).resize(function() {
-        if ($(window).width() < 1070) {
-          $(".icon-show").removeClass('d-none');
-
-        } else {
-            $(".icon-show").addClass('d-none');
-
-        }
-    }).resize();
-    </script>
-    <script type="text/javascript">
-    $(document).ready(function(){
-      $('#datatable_filter').parent().remove();
-    });
-    </script>
-    <script type="text/javascript">
-    $(document).ready(function(){
-      $('input#myInputTextField').on("focus", function(){
-        if ($(this).hasClass("searchActive")){
-               $(this).removeClass("searchActive");
-           }
-           else{
-          $('input#myInputTextField').addClass('searchActive');
-          }
-    });
-    });
-    </script>
+    
 
         @stop
