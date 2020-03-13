4de07a71b60c61d921184e0458033be2e4f42369
alirahmani <cyber.injector@yahoo.com>
2020-03-13 15:57:43 +0330

    api routes has been updated

--- routes/api.php
+++ routes/api.php
@@ -14,14 +14,34 @@ use Illuminate\Http\Request;
 */
 
 
-Route::get('/paymentHelper', function(Request $request){
-    return \Auth::user()->with('cards.bank', 'wallets', 'gateways' , 'checkouts.card', 'checkouts.wallet')->get()->toArray();
-})->middleware('auth:api');
+//Route::get('/paymentHelper', function(Request $request){
+//    return \Auth::user()->with('cards.bank', 'wallets', 'gateways' , 'checkouts.card', 'checkouts.wallet')->get()->toArray();
+//})->middleware('auth:api');
 
 
 Route::namespace('Api')->group(function () {
     Route::post('/login', 'AuthController@login');
     Route::get('/user/{api_token}', 'AuthController@user');
 
+
+
+    Route::get('/products/{shop_id}', function (Request $request) {
+        $products = \App\Product::where('shop_id', $request->shop_id)->limit(50)->get();
+        return \Response::json($products);
+    });
+
+    Route::get('/product/{product_id}', function (Request $request) {
+        $product = \App\Product::where('id', $request->product_id)->first();
+        return \Response::json($product);
+    });
+
+
+    Route::get('/categories/{shop_id}', function (Request $request) {
+        $categories = \App\ProductCategory::with('children')->where('shop_id', $request->shop_id)->where('parent_id', null)->get();
+        return \Response::json($categories);
+    });
+
+
 });
+
 
