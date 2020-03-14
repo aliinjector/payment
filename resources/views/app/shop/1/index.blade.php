0b78664e7b00374d2d162bcbdf3c500beb466a35
Hassan <hassankhosrojerdi@yahoo.com>
2020-03-14 15:09:43 +0330

    special offer theme 1

--- resources/views/app/shop/1/index.blade.php
+++ resources/views/app/shop/1/index.blade.php
@@ -122,27 +122,7 @@
 </div>
 
 
-<div class="row">
-    <div class="col-12">
-        <div class="card offer-box">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-md-4 mx-auto">
-                        <div class="offer-content text-center justify-content-center">
-                            <p class="text-white">{{ __('app-shop-1-index.pishnahadVizhe') }}</p>
-                            <h3 class="mb-3 text-white">50%{{ __('app-shop-1-index.sarfejooeeDarHazineha') }}</h3>
-                            <hr class="thick">
-                            <h5 class="text-white iranyekan">{{ __('app-shop-1-index.baKharideOnline') }}</h5>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!--end card-body-->
-        </div>
-        <!--end card-->
-    </div>
-    <!--end col-->
-</div>
+@includeWhen($shop->special_offer == 'enable','app.shop.1.layouts.partials.special_offer', ['special_text' => $shop->special_offer_text])
 
 
 <h2 class="my-5 line-throw"><span>{{ __('app-shop-1-index.porFrooshTarinHa') }}</span></h2>
