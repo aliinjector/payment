d233da5440489affb486a65b6dadff706c959af9
Farzaneh <farzane.a.shirazi@gmail.com>
2020-04-15 11:50:33 +0430

    feedback is added

--- resources/views/app/shop/4/index.blade.php
+++ resources/views/app/shop/4/index.blade.php
@@ -1439,8 +1439,8 @@
                     <div class="col-12">
                         <!-- section title start -->
                         <div class="section-title text-center">
+
                             <h2 class="title">بازخورد مشتریان</h2>
-                            <p class="sub-title"> توضیحات  توضیحات  توضیحات  توضیحات  توضیحات </p>
                         </div>
                         <!-- section title start -->
                     </div>
@@ -1463,10 +1463,11 @@
                                 </div>
                             </div>
                         </div>
+                          @foreach($feedbacks as $feedback)
                         <div class="testimonial-content-wrapper">
                             <div class="testimonial-content-carousel">
                                 <div class="testimonial-content">
-                                    <p>Vivamus a lobortis ipsum, vel condimentum magna. Etiam id turpis tortor. Nunc scelerisque, nisi a blandit varius, nunc purus venenatis ligula, sed venenatis orci augue nec sapien. Cum sociis natoque</p>
+                                    <p>{{ $feedback->feedback }}</p>
                                     <div class="ratings">
                                         <span><i class="fa fa-star-o"></i></span>
                                         <span><i class="fa fa-star-o"></i></span>
@@ -1474,7 +1475,7 @@
                                         <span><i class="fa fa-star-o"></i></span>
                                         <span><i class="fa fa-star-o"></i></span>
                                     </div>
-                                    <h5 class="testimonial-author">lindsy niloms</h5>
+                                    <h5 class="testimonial-author">{{ $feedback->fullName }}</h5>
                                 </div>
                                 <div class="testimonial-content">
                                     <p>Vivamus a lobortis ipsum, vel condimentum magna. Etiam id turpis tortor. Nunc scelerisque, nisi a blandit varius, nunc purus venenatis ligula, sed venenatis orci augue nec sapien. Cum sociis natoque</p>
@@ -1487,30 +1488,10 @@
                                     </div>
                                     <h5 class="testimonial-author">Daisy Millan</h5>
                                 </div>
-                                <div class="testimonial-content">
-                                    <p>Vivamus a lobortis ipsum, vel condimentum magna. Etiam id turpis tortor. Nunc scelerisque, nisi a blandit varius, nunc purus venenatis ligula, sed venenatis orci augue nec sapien. Cum sociis natoque</p>
-                                    <div class="ratings">
-                                        <span><i class="fa fa-star-o"></i></span>
-                                        <span><i class="fa fa-star-o"></i></span>
-                                        <span><i class="fa fa-star-o"></i></span>
-                                        <span><i class="fa fa-star-o"></i></span>
-                                        <span><i class="fa fa-star-o"></i></span>
-                                    </div>
-                                    <h5 class="testimonial-author">Anamika lusy</h5>
-                                </div>
-                                <div class="testimonial-content">
-                                    <p>Vivamus a lobortis ipsum, vel condimentum magna. Etiam id turpis tortor. Nunc scelerisque, nisi a blandit varius, nunc purus venenatis ligula, sed venenatis orci augue nec sapien. Cum sociis natoque</p>
-                                    <div class="ratings">
-                                        <span><i class="fa fa-star-o"></i></span>
-                                        <span><i class="fa fa-star-o"></i></span>
-                                        <span><i class="fa fa-star-o"></i></span>
-                                        <span><i class="fa fa-star-o"></i></span>
-                                        <span><i class="fa fa-star-o"></i></span>
-                                    </div>
-                                    <h5 class="testimonial-author">Maria Mora</h5>
-                                </div>
+                            
                             </div>
                         </div>
+                          @endforeach
                     </div>
                 </div>
             </div>
